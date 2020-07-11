86b0e951ae649f033a3fcd06bfd80b6d89f95041
aubehub <albalopsanchez@gmail.com>
2020-07-11 14:20:13 -0300

    Updates

---
+++ precache-manifest.a63ac8b597b333694b9d2d4471a97e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057c0ecb10097cf4f6a66b23b148e3c6",
+    "url": "/tick-tack-toe/index.html"
+  },
+  {
+    "revision": "6e2c2c3638a659df5b50",
+    "url": "/tick-tack-toe/static/css/main.4943255b.chunk.css"
+  },
+  {
+    "revision": "e461460efe5b9119b2e5",
+    "url": "/tick-tack-toe/static/js/2.688bde52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tick-tack-toe/static/js/2.688bde52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2c2c3638a659df5b50",
+    "url": "/tick-tack-toe/static/js/main.2a448f7a.chunk.js"
+  },
+  {
+    "revision": "d917d5a8c95137c07440",
+    "url": "/tick-tack-toe/static/js/runtime-main.6e07d2c3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tick-tack-toe/precache-manifest.a63ac8b597b333694b9d2d4471a97e60.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tick-tack-toe/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
